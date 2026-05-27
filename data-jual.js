d459314dcb03cdb2070fea21bb25d60fa687f379
a20574087-blip <a20574087@gmail.com>
2026-05-27 13:50:14 +0700

    Add files via upload

---
+++ data-jual.js
@@ -0,0 +1,23 @@
+const dataJual = [
+  {
+    nama: "Akun FF S1 SG2",
+    harga: "Rp150.000",
+    login: "google",
+    deskripsi: "Akun SG2, SG Lengket, SG Naga. Data aman, full gmail. Rekber ready.",
+    gambar: [
+      "https://files.catbox.moe/abc123.jpg",
+      "https://files.catbox.moe/def456.jpg"
+    ]
+  },
+  {
+    nama: "Akun ML Ex-Glory",
+    harga: "Rp300.000",
+    login: "moonton",
+    deskripsi: "Akun ML skin kolektor, wr 70%+, emblem max. Full gmail pribadi.",
+    gambar: [
+      "https://files.catbox.moe/xyz123.jpg",
+      "https://files.catbox.moe/xyz456.jpg",
+      "https://files.catbox.moe/xyz789.jpg"
+    ]
+  }
+];
---
+++ data-rental.js
@@ -0,0 +1,33 @@
+const dataRental = [
+  {
+    nama: "Akun FF S5+",
+    harga: "Rp15.000 / 3 Hari",
+    login: "google",
+    deskripsi: "Akun siap pakai, skin lumayan, rank Heroic. Cocok buat push rank.",
+    gambar: [
+      "https://files.catbox.moe/abc123.jpg",
+      "https://files.catbox.moe/def456.jpg"
+    ]
+  },
+  {
+    nama: "Akun ML Legend",
+    harga: "Rp20.000 / 5 Hari",
+    login: "moonton",
+    deskripsi: "Akun ML banyak skin legend, emblem max. Anti log out.",
+    gambar: [
+      "https://files.catbox.moe/xyz123.jpg",
+      "https://files.catbox.moe/xyz456.jpg",
+      "https://files.catbox.moe/xyz789.jpg"
+    ]
+  },
+    {
+    nama: "Akun FF S5+",
+    harga: "Rp15.000 / 3 Hari",
+    login: "bind",
+    deskripsi: "Akun siap pakai, skin lumayan, rank Heroic. Cocok buat push rank.",
+    gambar: [
+      "https://files.catbox.moe/abc123.jpg",
+      "https://files.catbox.moe/def456.jpg"
+    ]
+  }
+];
