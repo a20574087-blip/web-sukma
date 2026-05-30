aac5f53f2bc8451da7663ccc7e2cac5952e94e6c
a20574087-blip <a20574087@gmail.com>
2026-05-30 08:27:52 +0700

    Add files via upload

---
+++ data-payment.js
@@ -0,0 +1,18 @@
+window.paymentData = {
+  "profile": "https://files.catbox.moe/6m6ii5.jpg",
+  
+  "qris": "qris.png", // file QRIS lu
+
+  "dana": {
+    "nama": "NUR FADILA",
+    "nomor": "083139227699"
+  },
+  "ovo": {
+    "nama": "GA PUNYA", 
+    "nomor": "GA PUNYA"
+  },
+  "gopay": {
+    "nama": "azam",
+    "nomor": "083139227699"
+  }
+};
